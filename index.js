4e02d32718dc1db71d94e5a216c101b30218d964
Olehovych <olehbolonnuj@gmail.com>
2023-01-31 20:48:29 +0200

    second commit

---
+++ index.js
@@ -0,0 +1,9 @@
+
+alert("Задумайте число!")
+alert("Помножте це число на 2!")
+alert("Додайте до цого числа 7!")
+
+const result = prompt("Введи що вийшло");
+let userNumber = (result - 7) / 2;
+
+alert(`Ви задумали число  ${userNumber}`)
